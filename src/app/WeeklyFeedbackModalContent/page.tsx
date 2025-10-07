121b099ab715e4937b8d3ebf70ccd4966f89b921
Jimin0430 <tangbole0430@gmail.com>
2025-10-08 02:55:32 +0900

    ✨ Add dev page for WeeklyFeedbackModalContent preview

---
+++ src/app/WeeklyFeedbackModalContent/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useState } from "react";
+import WeeklyFeedbackModalContent from "../components/WeeklyFeedbackModalContent"
+
+export default function page () {
+  const [isClose, setIsClose] = useState(false);
+
+  const closeModal = () => {
+    setIsClose(false);
+  }
+  return (
+    <WeeklyFeedbackModalContent onClose={closeModal} villageName={'뭐시기 마을'}/>
+  )
+}

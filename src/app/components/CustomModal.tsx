e96eabf9ec6cd923bfe123ffa458831e7a904758
Jimin0430 <tangbole0430@gmail.com>
2025-10-08 02:15:47 +0900

    ✨ Make example modal a reusable component

--- src/app/components/CustomModal.tsx
+++ src/app/components/CustomModal.tsx
@@ -2,8 +2,6 @@
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
 const style = {
@@ -18,29 +16,23 @@ const style = {
   p: 4,
 };
 
-export default function CustomModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+interface CustomModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    children: React.ReactNode;
+}
 
+export default function CustomModal({ isOpen, onClose, children }: CustomModalProps) {
   return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
+    <Modal
+      open={isOpen}
+      onClose={onClose}
+      aria-labelledby="custom-modal-title"
+      aria-describedby="custom-modal-description"
+    >
+      <Box sx={style}>
+        {children}
+      </Box>
+    </Modal>
   );
 }

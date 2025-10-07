3a128e25c6325dbcfe00203185fe33f84a242f1d
Jimin0430 <tangbole0430@gmail.com>
2025-10-08 02:39:19 +0900

    ✨ Add home page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import React, { useCallback, useState } from 'react';
+import BottomSheet from '../components/BottomSheet';
+import TodoListContent from '../components/TodoListContent';
+import CityAreaContents from '../components/CityAreaContents';
+import { BottomSheetProvider } from '../context/BottomSheetContext';
+import CustomModal from '../components/CustomModal';
+import { TodoDetailModalContent } from '../components/TodoDetailModalContent';
+import VillageDetailModalContent from '../components/VillageDetailModalContent';
+import NewGoalCreationModalContent from '../components/NewGoalCreationModalContent';
+
+export default function Home() {
+  const [isSheetOpen, setIsSheetOpen] = useState(true);
+  const [currentSnapRatio, setCurrentSnapRatio] = useState(0.04);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState(null);
+
+  const handleTodoClick = useCallback((todo: any) => {
+    setSelectedTodo(todo);
+    setIsModalOpen(true);
+  }, []);
+
+  const [isVillageModalOpen, setIsVillageModalOpen] = useState(false);
+  const [selectedVillage, setSelectedVillage] = useState('');
+
+  const onVillageClick = useCallback((village: string) => {
+    setSelectedVillage(village);
+    setIsVillageModalOpen(true);
+  }, []);
+
+    const [isActionCreateModalOpen, setIsActionCreateModalOpen] = useState(false);
+
+  const onActionCreationClick = useCallback(() => {
+    setIsActionCreateModalOpen(true);
+  }, []);
+
+  const handleCloseSheet = () => setIsSheetOpen(false);
+  const handleCloseModal = () => setIsModalOpen(false);
+  const handleCloseVillageModal = () => setIsVillageModalOpen(false);
+  const handleActionCreationClick = () => setIsActionCreateModalOpen(false);
+
+  const snapPoints = [0.04, 0.35, 1];
+  const initialSnap = snapPoints.findIndex(p => p === currentSnapRatio);
+
+  return (
+    <div className="relative h-screen w-screen overflow-hidden">
+      <BottomSheetProvider currentSnapRatio={currentSnapRatio}>
+        {/* INFO:
+          시트가 올라와도, CityAreaContents는 항상 전체 화면에 렌더링됨
+          바텀 시트가 차지하는 만큼 시트 아래에 겹쳐짐 */}
+        <CityAreaContents onVillageClick={onVillageClick}/>
+        <BottomSheet
+          isOpen={isSheetOpen}
+          onClose={handleCloseSheet}
+          snapPoints={snapPoints}
+          initialSnap={initialSnap}
+          onSnapChange={setCurrentSnapRatio}
+        >
+          <TodoListContent onTodoClick={handleTodoClick} onCreateClick={onActionCreationClick}/>
+        </BottomSheet>
+      </BottomSheetProvider>
+      <CustomModal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <TodoDetailModalContent
+          todo={selectedTodo}
+          onClose={handleCloseModal}
+        />
+      </CustomModal>
+      <CustomModal isOpen={isVillageModalOpen} onClose={handleCloseVillageModal}>
+        <VillageDetailModalContent
+          village={selectedVillage}
+          onClose={handleCloseVillageModal}
+        />
+      </CustomModal>
+      <CustomModal isOpen={isActionCreateModalOpen} onClose={handleActionCreationClick}>
+        <NewGoalCreationModalContent
+          onClose={handleActionCreationClick}
+        />
+      </CustomModal>
+    </div>
+  );
+}
